250686c449ab54e03c1bd10fbd81f9a4df844880
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2024-02-12 12:03:58 -0800

    Start working

--- my_portfolio/app/page.tsx
+++ my_portfolio/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <div>Hello world</div>;
+    </div>
+  );
 }
 
 export default Home;
